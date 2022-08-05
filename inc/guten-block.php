1d358f50f81808ade20910a4cb4fee0b69687caa
Mike <mike@192.168.102.32>
2022-08-05 11:28:48 +0700

    New block blog posts

--- inc/guten-block.php
+++ inc/guten-block.php
@@ -3,22 +3,87 @@
  * ACF Gutenberg Block Register 
  * 
  */
-if( function_exists('acf_register_block') ) {
-	
-	$result = acf_register_block(array(
-    'mode' => 'edit',
+
+add_action('acf/init', function() {
+	if( !function_exists('acf_register_block') ) return;
+
+	acf_register_block([
+		'mode' => 'edit',
 		'name' => 'compare_advanced',
 		'title' => __('Compare Advanced', 'compare-advanced'),
 		'description' => __('Compare table block.', 'compare-advanced'),
 		'render_callback'	=> 'ca_compare_advanced_block_html',
-		//'category' => '',
-		//'icon' => '',
 		'keywords' => ['compare'],
-	));
-}
+	]);
+
+	acf_register_block([
+		'mode' => 'edit',
+		'name' => 'ca_image_block',
+		'title' => __('Image Block (CA)', 'compare-advanced'),
+		'description' => __('Image block.', 'compare-advanced'),
+		'render_callback'	=> 'ca_image_block_html',
+		'keywords' => ['image'],
+	]);
+
+	acf_register_block([
+		'mode' => 'edit',
+		'name' => 'ca_blog_posts',
+		'title' => __('Blog Posts (CA)', 'compare-advanced'),
+		'description' => __('Blog posts block.', 'compare-advanced'),
+		'render_callback'	=> 'ca_blog_posts_html',
+		'keywords' => ['blog', 'posts'],
+	]);
+});
 
 function ca_compare_advanced_block_html() {
   $items = get_field('compare_items');
   $extra_class = get_field('extra_class');
   echo do_shortcode('[compare_advanced compare_items="'. implode(',', $items) .'" extra_class="'. $extra_class .'" ]');
+}
+
+function ca_image_block_html() {
+	$image = get_field('image');
+	$label = get_field('label');
+	$url = get_field('url');
+  $custom_class = get_field('custom_class');
+	?>
+	<div class="ca-image-block <?php echo $custom_class; ?>">
+		<div class="image-layer" style="background: url(<?php echo $image['url'] ?>) no-repeat center center / cover, #333;"></div>
+		<div class="ca-image-block--inner">
+			<a href="<?php echo $url; ?>"><?php echo $label; ?></a>
+		</div>
+	</div>
+	<?php 
+}
+
+function ca_blog_posts_html() {
+	$number_posts = get_field('number_posts');
+	$custom_class = get_field('custom_class');
+
+	$posts = get_posts([
+		'numberposts' => $number_posts,
+		'post_type' => 'post',
+		'post_status' => 'publish',
+	]);
+
+	if(!$posts && count($posts) <= 0) return;
+	?>
+	<div class="ca-blog-posts-block">
+		<ul>
+			<?php foreach($posts as $index => $p) : ?>
+			<li>
+				<?php // var_dump($p); ?>
+				<a class="__thumb" href="<?php echo get_the_permalink($p->ID); ?>">
+					<?php echo get_the_post_thumbnail($p->ID, 'post-thumbnail') ?>
+				</a>
+				<h4 class="__title">
+					<a href="<?php echo get_the_permalink($p->ID); ?>">
+						<?php echo $p->post_title; ?>
+					</a>
+				</h4>
+			</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+	<?php
 }
